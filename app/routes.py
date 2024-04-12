8ee8064f7b415af3c45783664bb7bd6f58b48bc8
ohio-coder <joannedavid2307@gmail.com>
2024-04-12 09:48:31 +0300

    first commit

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,21 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index')
-def index():
-    """Index URL"""
-    return render_template('index.html' , title="index page")
+@app.route('/shop')
+def home():
+    """Shop URL"""
+    return render_template('shop.html' , title="shop page")
 
-@app.route('/about-me')
-def about_me():
-    """About me URL"""
-    return render_template('about_me.html' , title="About me page")
+@app.route('/register')
+def register():
+    """Register URL"""
+    return render_template('register.html' , title="register page")
+@app.route('/login')
+def login():
+    """Login URL"""
+    return render_template('login.html' , title="login page")
+
+
+
 
 
